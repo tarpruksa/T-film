bda2e02dd0a7b4df10e5f2f08b2d022ada8341ef
tarpruksa <tar.violin@gmail.com>
2022-08-08 17:35:56 +0700

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
 import useStyles from './styles';
+import useAlan from './Alan';
 
-import { Actors, Movies, MovieInformation, NavBar, Profile } from './';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const App = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const alanBtnContainer = useRef();
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <NavBar />
-            <main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Switch>
-                    <Route exact path="/movies/:id">
-                        <MovieInformation />
-                    </Route>
-                    <Route exact path="/actors/:id">
-                        <Actors />
-                    </Route>
-                    <Route exact path="/">
-                        <Movies />
-                    </Route>
-                    <Route exact path="/profile/:id">
-                        <Profile />
-                    </Route>
-                </Switch>
-            </main>
-        </div>
-    )
-}
+  useAlan();
 
-export default App
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path={['/', '/approved']}>
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+      <div ref={alanBtnContainer} />
+    </div>
+  );
+};
+
+export default App;
